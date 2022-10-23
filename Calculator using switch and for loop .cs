0569c5c572476d74f07e55f7016c488ae31094bc
Imdadul Hoque <61014632+ImdadulhoqueMasud@users.noreply.github.com>
2022-10-23 23:45:46 +0600

    Add files via upload

---
+++ Calculator using switch and for loop .cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Math;
+class max
+{
+    static void Main(string[] args)
+    {
+        int secondNumber, firstNumber, result;
+        Console.Write("Give 1st Number : ");
+        firstNumber = Convert.ToInt32(Console.ReadLine());
+       for (; ; ) { 
+        Console.Write("Give 2nd Number : ");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Select the operation : ");
+        int selectNumber = Convert.ToInt32(Console.ReadLine());
+        switch (selectNumber)
+        {
+            case 0:
+                {
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine(result);
+                }
+                break;
+            case 1:
+                {
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine(result);
+                }
+                break;
+            case 2:
+                {
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine(result);
+                }
+                break;
+            case 3:
+                {
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine(result);
+                }
+                break;
+            default:
+                Console.WriteLine("END");
+                break;
+        }
+        }
+    }
+}
---
+++ two double number as input and convert them to integer then add these numbers .cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Task1
+{
+    class AddTwoNumber
+    {
+        static void Main()
+        { 
+            Console.Write("1st Numer :");
+            double fistNumer = Convert.ToDouble( Console.ReadLine());
+            Console.Write("2nd Numer :");
+            double secendNumber=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Show the\n1st Number:" + fistNumer+ "\n2nd Numer: " +secendNumber);
+            int convertFisrtNmber = Convert.ToInt32(fistNumer);
+            Console.WriteLine("convert the input number: ");
+            Console.WriteLine("convert 1st input: " +convertFisrtNmber);
+           int convertSecendNmber = Convert.ToInt32(secendNumber);
+            Console.WriteLine("convert 2nd input: " + convertSecendNmber);
+            Console.WriteLine("add to number : ");
+            int result = (convertFisrtNmber + convertSecendNmber);
+             Console.WriteLine(convertFisrtNmber + "+"+ convertSecendNmber + "=" + result);
+        }
+    }
+}
